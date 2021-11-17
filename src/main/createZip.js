3d3d58c96c6b57c0980a83913edb3243345a630e
unknown <andbrazhnikov@gmail.com>
2021-11-17 14:24:40 +0300

    Zip creation fixed

--- src/main/createZip.js
+++ src/main/createZip.js
@@ -1,13 +1,15 @@
 import createXml from './xml/createXml';
 import AdmZip from 'adm-zip';
+import { format } from 'date-fns';
+import fs from 'fs';
 
 export default async (xml) => {
     const zip = new AdmZip();
     const date = format(new Date(), 'yyMMdd');
     await createXml(xml, date);
-    const stat = fs.statSync(`C:/Users/User/Desktop/Выгрузка ФФОМС//FM990089F00${date}.xml`);
+    const stat = fs.statSync(`C:/Users/User/Desktop/Выгрузка ФФОМС/FM990089F00${date}.xml`);
     console.log(stat)
-    const add = await zip.addLocalFile(`C:/Users/User/Desktop/Выгрузка ФФОМС//FM990089F00${date}.xml`);
+    const add = await zip.addLocalFile(`C:/Users/User/Desktop/Выгрузка ФФОМС/FM990089F00${date}.xml`);
     console.log(add)
     await zip.writeZip(`C:/Users/User/Desktop/Выгрузка ФФОМС/FM990089F00${date}.zip`);
     return true
--- src/main/xml/createXml.js
+++ src/main/xml/createXml.js
@@ -8,12 +8,9 @@ export default async (xml, date) => {
   await writeFile(`${__dirname}/file.xml`, xml);
   return new Promise((resolve) => {
     const write = fs.createWriteStream(`C:/Users/User/Desktop/Выгрузка ФФОМС/FM990089F00${date}.xml`);
-    const readable =fs.createReadStream(`${__dirname}/file.xml`)
+    const readable = fs.createReadStream(`${__dirname}/file.xml`)
       .pipe(iconv.decodeStream('utf8'))
       .pipe(iconv.encodeStream('win1251'))
-      .pipe(fs.createWriteStream(`C:/Users/User/Desktop/Выгрузка ФФОМС/FM990089F00${date}.xml`))
-      .on('end', () => { console.log('end')});
-    console.log(readable)
-    write.on('finish', resolve);
-  });
-};
+      .pipe(write)
+      .on('close', () => resolve(`C:/Users/User/Desktop/Выгрузка ФФОМС/FM990089F00${date}.xml created`));
+})};
